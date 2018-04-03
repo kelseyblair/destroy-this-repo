d442612dc62d0334660b625cb311e7c698688633
Kelsey Blair <kelseyblair@outlook.com>
2018-04-02 22:54:39 -0700

    minor changes to fizzbuzz output

--- src/fizzbuzz.c
+++ src/fizzbuzz.c
@@ -7,9 +7,11 @@ int main()
 	for( int i = 1; i <= 100; i++ )
 	{
 		if( i%3 == 0 && i%5 == 0 ) { printf("fizzbuzz\n"); }
-		else if( i%3 == 0 ) { printf("fizz\n"); }
-		else if( i%5 == 0 ) { printf("buzz\n"); }
-		else { printf("%d\n", i ) ; }
+		else if( i%3 == 0 ) { printf("fizz "); }
+		else if( i%5 == 0 ) { printf("buzz "); }
+		else { printf("%d ", i ) ; }
 	}
+        
+        printf("\n");
 }
 
